6fe47537c69f1fca68071c2d40818e5d060bd1e5
MusarathAli <47379433+MusarathAli@users.noreply.github.com>
2019-04-30 00:06:51 +0530

    Update app.js

--- GulpJSBeginners-master/src/js/app.js
+++ GulpJSBeginners-master/src/js/app.js
@@ -6,5 +6,21 @@ function mul(a, b)
 {
    return a * b;
 }
+
+// var element = document.getElementsByClassName("item-products");
+	
+// declare function for "onMouseOver" event:
+ function bigImg(element) {
+    element.classList.add("active");
+   var ele = document.querySelector("header");
+   ele.className= "globalNav initialized overlayActive dropdownActive";
+ }
+ function bigImg(element) {
+    element.classList.add("active");
+   var ele = document.querySelector("header");
+   ele.className= "globalNav initialized overlayActive dropdownActive";
+ }
+
+
 console.log(add(5,6));
 console.log(mul(5,6));
